ab565aafb1a4d8e78ed915b92cea59b4f91d894f
Lucas Lopes Mercini <lucas.mercini@maggie.synergia.dcc.ufmg.br>
2026-06-24 08:32:33 -0300

    Trabalho dia 23-04-2026

--- imports/modules/example/api/exampleServerApi.ts
+++ imports/modules/example/api/exampleServerApi.ts
@@ -4,6 +4,7 @@ import { exampleSch, IExample } from './exampleSch';
 import { userprofileServerApi } from '../../../modules/userprofile/api/userProfileServerApi';
 import { ProductServerBase } from '../../../api/productServerBase';
 import { IUserProfile } from '../../userprofile/api/userProfileSch';
+import { Meteor } from 'meteor/meteor';
 
 // endregion
 
@@ -14,78 +15,91 @@ class ExampleServerApi extends ProductServerBase<IExample> {
 			// saveImageToDisk: true,
 		});
 
+
 		const self = this;
 
-
 		// PAREI AQUI
-		/*
-		this.addTransformedPublication(
-			'exampleDetail',
-			async (filter = {}) =>{
-				return this.find(
-					filter,{
-						fields:{
-							title :1,
-							type : 1,
-							description: 1,
-							typeMulti:1,
-							check:1, 
-							createdby: 1,
-							author: 1,
-							statusConcluded:1,
-							statusToggle: 1,
-				
-						}
 
-					});
-			},
-			async (doc: Partial<IExample>) : Promise<Partial<IExample>> =>{
-				const user: IUserProfile = await userprofileServerApi.getCollectionInstance().findOneAsync({
-					_id: doc.createdby}, 
-					{
-					fields:{username: 1 }
-				})
-				return {...doc, username : user?.username || "Erro"};			
-			}
-		)*/
+		this.addTransformedPublication(
+            'exampleDetail',
+            (filter = {}) => {
+                return this.defaultDetailCollectionPublication(filter, {
+                    projection: {
+                        contacts: 1,
+						title:1,
+						description: 1,
+                        type: 1,
+                        typeMulti: 1,
+                        date: 1,
+                        files: 1,
+                        chip: 1,
+                        statusRadio: 1,
+                        statusToggle: 1,
+                        slider: 1,
+                        check: 1,
+                        address: 1,
+                        statusConcluded: 1,
+                        nome: 1,
+                        author: 1,
+                        createdby: 1 
+                    }
+                });
+            },
+            async (doc: Partial<IExample>) : Promise<Partial<IExample>> => {
+                if (!doc.createdby) {
+                    return { ...doc, username: "Sem Autor" };
+                }
+                const user: IUserProfile = await userprofileServerApi.getCollectionInstance().findOneAsync(
+                    { _id: doc.createdby }, 
+                    { fields: { username: 1 } }
+                );
+                return { ...doc, username: user?.username || "Desconhecido" };           
+            }
+        );
 		
 
 		this.addTransformedPublication(
-			'exampleList',
-			(filter = {}) => {
-				return this.defaultListCollectionPublication(filter, {
-					projection: { title: 1, type: 1, typeMulti: 1, 
-						createdat: 1, statusConcluded: 1, nome: 1, check: 1, author: 1, statusToggle: 1 
-						}
-				});
-			},
-			async (doc: IExample & { nomeUsuario: string }) => {
-				const userProfileDoc = await userprofileServerApi.getCollectionInstance().findOneAsync({ _id: doc.createdby });
-				return { ...doc };
-			}
-		);
+  'exampleList',
+  (filter = {}) => {
 
-		this.addTransformedPublication(
-			'exampleDetail',
-			async (filter = {}) => {
-				return this.find(filter, {
-					fields: {
-						title: 1, type: 1, description: 1, typeMulti: 1, check: 1, 
-						createdby: 1, author: 1, statusConcluded: 1, statusToggle: 1,
-						contacts: 1, date: 1, files: 1, chip: 1, statusRadio: 1, 
-						slider: 1, address: 1, nome: 1
-					}
-				});
-			},
-			async (doc: Partial<IExample>) : Promise<Partial<IExample>> => {
-				const user: IUserProfile = await userprofileServerApi.getCollectionInstance().findOneAsync(
-					{ _id: doc.createdby }, 
-					{ fields: { username: 1 } }
-				);
-				return { ...doc, username: user?.username || "Desconhecido" };          
-    }
-);
+	const userId = Meteor.userId();
+	console.log(userId)
+    const personalFilter = {
+      ...filter,
+	  $or:[
+		{statusToggle: false},
+		{createdby: userId}
+		
+	  ]
+    	
+    };
+
 	
+    return this.defaultListCollectionPublication(personalFilter, {
+      projection: {
+		title:1,
+        type: 1,
+        typeMulti: 1,
+        createdat: 1,
+        statusConcluded: 1,
+        nome: 1,
+        check: 1,
+        statusToggle: 1,
+        author: 1,
+      },
+    });
+  },
+  async (doc: IExample & { nomeUsuario: string }) => {
+    const userProfileDoc =
+      await userprofileServerApi.getCollectionInstance().findOneAsync({
+        _id: doc.createdby,
+      });
+
+    return { ...doc };
+  }
+);
+
+
 	}
 }
 
